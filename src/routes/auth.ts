9b7d818b8fa6adf4ec773b991481c4ff7edf9231
ronald-assis <ronaldassis71@gmail.com>
2023-09-21 14:43:21 -0300

    feat: config access token response to github

---
+++ src/routes/auth.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from 'fastify'
+import axios from 'axios'
+import { z } from 'zod'
+
+export async function authRoutes(app: FastifyInstance) {
+  app.post('/register', async (request) => {
+    const bodySchema = z.object({
+      code: z.string(),
+    })
+
+    const { code } = bodySchema.parse(request.body)
+
+    const accessTokenResponse = await axios.post(
+      'http://github.com/login/oauth/access_token',
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    )
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 const PORT = 3333
@@ -9,6 +10,7 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app

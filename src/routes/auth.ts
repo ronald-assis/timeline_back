b0c40911d9f8ad0f3c366a1399a5d9215b2ac372
ronald-assis <ronaldassis71@gmail.com>
2023-09-22 14:51:07 -0300

    feat: authentication route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import axios from 'axios'
 import { z } from 'zod'
+import { prisma } from '../lib/prisma'
 
 export async function authRoutes(app: FastifyInstance) {
   app.post('/register', async (request) => {
@@ -24,5 +25,43 @@ export async function authRoutes(app: FastifyInstance) {
         },
       },
     )
+
+    const { access_token } = accessTokenResponse.data
+
+    const userResponse = await axios.get('http://api.github.com/user', {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    })
+
+    const userSchema = z.object({
+      id: z.number(),
+      login: z.string(),
+      name: z.string(),
+      avatar_url: z.string().url(),
+    })
+
+    const { id, login, name, avatar_url } = userSchema.parse(userResponse.data)
+
+    let user = await prisma.user.findUnique({
+      where: {
+        githubId: id,
+      },
+    })
+
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          githubId: id,
+          login,
+          name,
+          avatarUrl: avatar_url,
+        },
+      })
+    }
+
+    return {
+      user,
+    }
   })
 }

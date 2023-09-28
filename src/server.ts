7205af8de3905dacc0f2bc583b2a9e832333c698
ronald-assis <ronaldassis71@gmail.com>
2023-09-28 11:23:19 -0300

    feat: authentication routes, upload route edited gitignore file

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
+import { resolve } from 'node:path'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import fastifyStatic from '@fastify/static'
 
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
 const PORT = 3333
+
+app.register(multipart)
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -17,11 +27,13 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: PORT,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log(`HTTP server running on http://localhost:${PORT}`)

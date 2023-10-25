c1d0bdbd3339215a4a0c354ceea4e65ff695f67f
ronald-assis <ronaldassis71@gmail.com>
2023-10-25 17:11:40 -0300

    feat: adding key to list

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -22,6 +22,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
       id: memory.id,
       converUrl: memory.coverUrl,
       excerpt: memory.content.substring(0, 115).concat('...'),
+      createdAt: memory.createdAt,
     }))
   })
 
